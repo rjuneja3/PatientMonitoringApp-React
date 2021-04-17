50d5d20f0fe884ce444997e5f275323f3c01e269
Dinara <inraidi@gmail.com>
2021-04-17 03:56:04 -0400

    More UI

--- tired_devs_comp308project/react-client/src/components/Footer.js
+++ tired_devs_comp308project/react-client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function Footer() {
   return (
-    <footer className="py-3 bg-light text-primary-50 fixed-bottom">
+    <footer className="py-3 text-primary-50 fixed-bottom footer">
     <div className="container text-center">
       <small>Copyright &copy; TIRED DEVS </small>
     </div>
